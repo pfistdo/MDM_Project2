a06fcf2bc546621f54659b4f7206836745723886
pfistdo <pfistdo1@students.zhaw.ch>
2023-05-16 17:36:51 +0200

    Implemented predict endpoint

--- src/main/java/ch/zhaw/pfistdo1/mdm/project2/controller/ServicesController.java
+++ src/main/java/ch/zhaw/pfistdo1/mdm/project2/controller/ServicesController.java
@@ -1,10 +1,20 @@
 package ch.zhaw.pfistdo1.mdm.project2.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import ai.onnxruntime.OrtException;
+import ch.zhaw.pfistdo1.mdm.project2.model.AgeClassifier;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class ServicesController {
@@ -19,9 +29,15 @@ public class ServicesController {
         return "Sentiment app is up and running!";
     }
 
-    @GetMapping("/ageClassifier")
-    public String predict(@RequestParam(name = "text", required = true) String text) {
-        return "Age: ";
+    @PostMapping("/ageClassifier")
+    public String predict(@RequestParam("image") MultipartFile imageFile) {
+        try {
+            BufferedImage image = ImageIO.read(imageFile.getInputStream());
+            return AgeClassifier.getAge(image);
+        } catch (IOException | OrtException e) {
+            e.printStackTrace();
+            return "Error processing the image.";
+        }
     }
 
 }
